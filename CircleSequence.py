d74fa12295cf454df6b05b801c5178ab13956ae0
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-01 19:29:27 +0100

    Remove temporary code

--- CircleSequence.py
+++ CircleSequence.py
@@ -1,18 +1,3 @@
-"""
-Create a new CircleSequence class.
-It should accept an iterable, which represents our data source
-and a number, which tells us how many items should be produced.
-
-For example
-
-c = CircleSequence('xyz', 5)
-print(list(c))        # prints x, y, z, x, y
-
-c2 = CircleSequence([1,2], 5)
-print(list(c2))        # prints 1,2,1,2,1
-"""
-
-
 class CircleSequence:
 
     def __init__(self, iterable, number):

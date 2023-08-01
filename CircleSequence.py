b8f8ed8c1b2ee5d4b9d6ad6a6f10719cae5f4a49
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-01 19:21:03 +0100

    Unit test with compile error

---
+++ CircleSequence.py
@@ -0,0 +1,13 @@
+"""
+Create a new CircleSequence class.
+It should accept an iterable, which represents our data source
+and a number, which tells us how many items should be produced.
+
+For example
+
+c = CircleSequence('xyz', 5)
+print(list(c))        # prints x, y, z, x, y
+
+c2 = CircleSequence([1,2], 5)
+print(list(c2))        # prints 1,2,1,2,1
+"""
---
+++ TestCircleSequence.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestCircleSequence(TestCase):
+
+    def test_with_string(self):
+        sequence = CircleSequence('xyz', 5)

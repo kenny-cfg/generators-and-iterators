94febf4b9e25d58d56acea70d24e507ada85fb5b
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-01 18:59:59 +0100

    Iterable from a set

---
+++ main2.py
@@ -0,0 +1,12 @@
+nums_iter = iter({1, 2, 3, 4, 5})
+
+for number in nums_iter:
+    print(number)
+# print(nums_iter.__next__())
+# print(nums_iter.__next__())
+# print(nums_iter.__next__())
+# print(nums_iter.__next__())
+# print(nums_iter.__next__())
+#
+# # this one will throw an error
+# print(nums_iter.__next__())
